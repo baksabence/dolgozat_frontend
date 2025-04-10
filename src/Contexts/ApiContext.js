beaa5373bb0e668e664de2a31853cd6d62edb4f7
Baksa Bence (SZF_2023_2025) <baksa.bence@diak.szamalk-szalezi.hu>
2025-04-10 17:19:26 +0200

    credentials

--- src/Contexts/ApiContext.js
+++ src/Contexts/ApiContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { myAxios } from "../Axios/axios";
+import { myAxios } from "../Axios/myAxios";
 
 const ApiContext = createContext();
 
